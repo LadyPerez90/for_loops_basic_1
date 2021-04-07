be333c16b9a008b3596c8a0381f748b35b2ab0c8
LadyPerez90 <josefa.groce@gmail.com>
2021-04-07 18:21:55 -0500

    assignment turn in

---
+++ for_loop_basic_1.py
@@ -0,0 +1,43 @@
+# Basic - Print all intergers from 0 to 150
+# def basic():
+#     for x in range(0,151,1):
+#       print(x)
+# basic()
+
+# Multiples of Five
+# def multi_five():
+#     for x in range(5,1001,5):
+#         print(x)
+# multi_five()
+
+# Counting, the Dojo Way
+# def dojo_way():
+#     for x in range(1,101,1):
+#         if(x%10==0):
+#             print("Coding Dojo")
+#         elif(x%5==0):
+#             print("Coding")
+# dojo_way()
+
+# Whoa. That Sucker's Huge
+# def huge_sum():
+#     sum = 0
+#     for x in range(0,500,1):
+#         if(x%2!=0):
+#             sum += x
+#     print(sum)
+# huge_sum()
+
+
+# Countdown by Fours
+# def by_four():
+#     for x in range (2018,0,-4):
+#         print(x)
+# by_four()
+
+# # Flexible Counter
+# def flex_count(low_num, high_num, mult):
+#     for x in range(low_num, high_num + 1):
+#         if x % mult ==0:
+#             print(x)
+# flex_count(2,9,3)
